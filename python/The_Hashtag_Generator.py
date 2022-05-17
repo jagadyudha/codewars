4e877cea8915ae6a3539d6c02d4499e7db3dd3d8
unknown <jagadyudhaawali@gmail.com>
2022-05-17 18:30:35 +0700

    fix: hashtag generator

--- python/The_Hashtag_Generator.py
+++ python/The_Hashtag_Generator.py
@@ -1,6 +1,16 @@
+# The marketing team is spending way too much time typing in hashtags.
+# Let's help them with our own Hashtag Generator!
 
+# Here's the deal:
 
-from numpy import empty
+# It must start with a hashtag (#).
+# All words must have their first letter capitalized.
+# If the final result is longer than 140 chars it must return false.
+# If the input or the result is an empty string it must return false.
+# Examples
+# " Hello there thanks for trying my Kata"  =>  "#HelloThereThanksForTryingMyKata"
+# "    Hello     World   "                  =>  "#HelloWorld"
+# ""                                        =>  false
 
 
 def generate_hashtag(s):
