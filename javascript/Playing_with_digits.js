0d782bd9a63825dd013cc54d491eeda86342a00d
Jagad Yudha <jagadyudhaawali@gmail.com>
2022-05-19 13:04:08 +0700

    feat(javasript): Playing with digits

---
+++ javascript/Playing_with_digits.js
@@ -0,0 +1,23 @@
+// Is there an integer k such as : (a ^ p + b ^ (p+1) + c ^(p+2) + d ^ (p+3) + ...) = n * k
+
+// If it is the case we will return k, if not return -1.
+
+// Note: n and p will always be given as strictly positive integers.
+
+// digPow(89, 1) should return 1 since 8¹ + 9² = 89 = 89 * 1
+// digPow(92, 1) should return -1 since there is no k such as 9¹ + 2² equals 92 * k
+// digPow(695, 2) should return 2 since 6² + 9³ + 5⁴= 1390 = 695 * 2
+// digPow(46288, 3) should return 51 since 4³ + 6⁴+ 2⁵ + 8⁶ + 8⁷ = 2360688 = 46288 * 51
+
+function digPow(n, p) {
+  const toArr = n.toString().split("");
+  let temp = 0;
+
+  for (let i = 0; i < toArr.length; i++) {
+    temp = Math.pow(toArr[i], p + i) + temp;
+  }
+
+  return (temp / n) % 1 == 0 ? temp / n : -1;
+}
+
+console.log(digPow(92, 1));
