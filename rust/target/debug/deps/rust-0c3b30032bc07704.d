d868a5394a5146ae4e6a5f96edc9270517b08ab8
Jagad Yudha Awali <jagadyudhaawali@gmail.com>
2023-05-22 21:36:27 +0700

    .gitignore is now working

--- rust/target/debug/deps/rust-0c3b30032bc07704.d
+++
@@ -1,6 +0,0 @@
-/Users/yudha/Projects/playground/codewars/rust/target/debug/deps/rust-0c3b30032bc07704.rmeta: src/main.rs src/bmi.rs
-
-/Users/yudha/Projects/playground/codewars/rust/target/debug/deps/rust-0c3b30032bc07704.d: src/main.rs src/bmi.rs
-
-src/main.rs:
-src/bmi.rs:
--- rust/target/debug/deps/rust-9bffeb7dd5306e3d.d
+++
@@ -1,6 +0,0 @@
-/Users/yudha/Projects/playground/codewars/rust/target/debug/deps/rust-9bffeb7dd5306e3d.rmeta: src/main.rs src/bmi.rs
-
-/Users/yudha/Projects/playground/codewars/rust/target/debug/deps/rust-9bffeb7dd5306e3d.d: src/main.rs src/bmi.rs
-
-src/main.rs:
-src/bmi.rs:
